219424a47e1ab1d2a4b7b342c7da878097e6408e
Miquel Garcia <miquel.garcia@soprasteria.com>
2025-12-04 15:02:25 +0100

    fix(service-worker): fix service worker

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -29,6 +29,11 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(cacheResp => cacheResp || fetch(event.request))
+    caches.match(event.request).then(cacheResp => {
+      return (
+        cacheResp ||
+        fetch(event.request).catch(() => caches.match('/index.html'))
+      );
+    })
   );
 });
